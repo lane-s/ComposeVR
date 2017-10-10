3f9d1aaf31253a610a935382db2aa063d7077594
lane-s <las4vc@virginia.edu>
2017-10-10 14:12:31 -0400

    Initial commit

---
+++ Assets/ComposeVR/Protocol.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Text;
+using UnityEngine;
+
+public class Protocol : MonoBehaviour {
+
+	public static byte[] packMessage(string message){
+		System.Int32 messageLength = message.Length;
+
+		byte[] header = BitConverter.GetBytes (messageLength);
+		if (BitConverter.IsLittleEndian)
+			Array.Reverse (header, 0, header.Length);
+		
+		byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+		byte[] buffer = new byte[header.Length + messageBytes.Length];
+		System.Buffer.BlockCopy (header, 0, buffer, 0, header.Length);
+		System.Buffer.BlockCopy (messageBytes, 0, buffer, header.Length, messageBytes.Length);
+
+		return buffer;
+	}
+}
