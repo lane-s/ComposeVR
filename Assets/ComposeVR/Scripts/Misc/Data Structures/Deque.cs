4aa8bdbae1c08b00395598516a28ac7e79387926
lane-s <las4vc@virginia.edu>
2018-04-19 22:51:45 -0400

    Add circular buffer based deque implementation
    The new note selector will need a data structure that allows for fast
    push/pop at both ends as well as access to the middle element. While a
    doubly-linked list would satisfy this requirement, a circular buffer
    based deque is more suited to the problem since the size will not need
    to change, so we can achieve zero allocation.

---
+++ Assets/ComposeVR/Scripts/Misc/Data Structures/Deque.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+/// <summary>
+/// Implements a deque using a circular buffer
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Deque<T> {
+
+    public int Count {
+        set { numElements = value; }
+        get { return numElements; }
+    }
+    private int numElements;
+
+    private T[] buffer;
+
+    private int front;
+    private int back;
+
+    public Deque(int size) {
+        buffer = new T[Math.Max(size, 1)];
+        front = -1;
+        back = 0;
+        numElements = 0;
+    }
+
+    public void PushFront(T item) {
+        if(numElements == buffer.Length) {
+            ResizeBuffer();
+        }
+        front = (front + 1) % buffer.Length;
+        buffer[front] = item;
+
+        numElements += 1;
+    }
+
+    public void PushBack(T item) {
+        if(numElements == buffer.Length){
+            ResizeBuffer();
+        }
+
+        back -= 1;
+        if(back < 0) {
+            back = buffer.Length - 1;
+        }
+        buffer[back] = item;
+
+        numElements += 1;
+    }
+
+    private void ResizeBuffer() {
+        T[] oldBuff = buffer;
+        buffer = new T[oldBuff.Length * 2];
+
+        for(int i = 0; i <= front; i++) {
+            buffer[front] = oldBuff[i];
+        }
+
+        if(front < back) {
+            int toEnd = oldBuff.Length - back;
+            for(int i = 0; i < toEnd; i++) {
+                buffer[buffer.Length - 1 - i] = oldBuff[oldBuff.Length - i - 1];
+            }
+        }
+    }
+
+    public T PopFront() {
+        if(numElements == 0) {
+            return default(T);
+        }
+
+        T frontElem = PeekFront();
+
+        buffer[front] = default(T);
+
+        front -= 1;
+        if(front < 0) {
+            front = buffer.Length - 1;
+        }
+
+        numElements -= 1;
+        return frontElem;
+    }
+
+    public T PopBack() {
+        if(numElements == 0) {
+            return default(T);
+        }
+
+        T backElem = PeekBack();
+
+        buffer[back] = default(T);
+        back = (back + 1) % buffer.Length;
+
+        numElements -= 1;
+        return backElem;
+    }
+
+    public T PeekFront() {
+        if(numElements == 0) {
+            return default(T);
+        }
+        return buffer[front];
+    }
+
+    public T PeekBack() {
+        if(numElements == 0) {
+            return default(T);
+        }
+        return buffer[back];
+    }
+
+    public T Get(int i) {
+        if(i < 0 || i >= numElements) {
+            return default(T);
+        }
+
+        int idx = front - i;
+        if(idx < 0) {
+            idx = buffer.Length + idx;
+        }
+
+        return buffer[idx];
+    }
+}
---
+++ Assets/ComposeVR/Scripts/Misc/Data Structures/DequeTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ComposeVR {
+    public class DequeTest : MonoBehaviour {
+
+        private const int N = 5;
+        Deque<int> testDeque;
+
+        // Use this for initialization
+        void Awake () {
+            testDeque = new Deque<int>(N);
+
+            //Test push, pop, and get when initial size is not exceeded
+            testDeque.PushFront(1);
+            testDeque.PushBack(-1);
+            testDeque.PushFront(2);
+            testDeque.PushFront(-2);
+            testDeque.PushFront(3);
+
+            Debug.Assert(testDeque.Get(0) == 3);
+            Debug.Assert(testDeque.Get(1) == -2);
+            Debug.Assert(testDeque.Get(2) == 2);
+            Debug.Assert(testDeque.Get(3) == 1);
+            Debug.Assert(testDeque.Get(4) == -1);
+
+            Debug.Assert(testDeque.PopFront() == 3);
+            Debug.Assert(testDeque.PopBack() == -1);
+            Debug.Assert(testDeque.PopBack() == 1);
+            Debug.Assert(testDeque.PopBack() == 2);
+            Debug.Assert(testDeque.PopFront() == -2);
+        }
+    }
+}
